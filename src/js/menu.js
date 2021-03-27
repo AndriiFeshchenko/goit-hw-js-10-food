c12f57bc8f7eb8434d89a68fe2774da7f029dd81
AndriiFeshchenko <71959542+AndriiFeshchenko@users.noreply.github.com>
2021-03-28 00:38:12 +0200

    Initial commit

---
+++ src/js/menu.js
@@ -0,0 +1,6 @@
+'use strict'
+import template from "../templates/menu-item.hbs";
+import content from "../data/menu.json";
+
+const container = document.querySelector(".js-menu");
+container.innerHTML = template(content);
