28d214e11765a24c896e82ee713d3dffd1e698a0
AndriiFeshchenko <71959542+AndriiFeshchenko@users.noreply.github.com>
2021-03-28 01:13:51 +0200

    Updates

--- src/index.js
+++
@@ -1,3 +0,0 @@
-import './css/styles.css';
-import './js/theme.js'
-import './js/menu.js'
--- src/js/menu.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-import template from "../templates/menu-item.hbs";
-import content from "../data/menu.json";
-
-const container = document.querySelector(".js-menu");
-container.innerHTML = template(content);
